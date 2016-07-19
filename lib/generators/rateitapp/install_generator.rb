258e58212cb46eb757d71a6585b799bd7764d538
nvanderlaan <nvanderlaan@mpr.org>
2016-07-19 13:22:52 -0500

    WIP: Update generator

--- lib/generators/rateitapp/install_generator.rb
+++ lib/generators/rateitapp/install_generator.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
+require 'rails/generators/active_record'
+require 'rails/generators/migration'
+
 module Rateitapp
   module Generators
     # Auto generate files in host app
-    class InitializerGenerator < Rails::Generators::Base
+    class InstallGenerator < Rails::Generators::Base
       def create_initializer_file
         create_file 'config/initializers/rateit.rb', '# Add initialization content here'
       end

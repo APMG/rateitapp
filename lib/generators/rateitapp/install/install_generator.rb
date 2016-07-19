dcad5b08ecd315328b54f8af05c697cc39c93a28
William Johnston <wjohnston@mpr.org>
2016-07-19 13:53:17 -0500

    Get generator working

---
+++ lib/generators/rateitapp/install/install_generator.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Rateitapp
+  # Install required configuration files.
+  class InstallGenerator < Rails::Generators::Base
+    def create_initializer_file
+      create_file 'config/initializers/rateit.rb', '# Add initialization content here'
+    end
+  end
+end

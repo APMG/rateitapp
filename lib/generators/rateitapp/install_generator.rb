dea7a1b32e2b5322e6db37e86e17cfcaabf37c54
nvanderlaan <nvanderlaan@mpr.org>
2016-07-19 13:27:44 -0500

    WIP: Update generator

--- lib/generators/rateitapp/install_generator.rb
+++ lib/generators/rateitapp/install_generator.rb
@@ -5,7 +5,7 @@ require 'rails/generators/migration'
 module Rateitapp
   module Generators
     # Auto generate files in host app
-    class InstallGenerator < Rails::Generators::Base
+    class RateitappGenerator < Rails::Generators::Base
       def create_initializer_file
         create_file 'config/initializers/rateit.rb', '# Add initialization content here'
       end

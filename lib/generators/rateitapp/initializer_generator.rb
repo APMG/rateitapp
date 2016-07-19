6a99fedfb19caa1c9bc37dadbb09fc148d17d7d1
nvanderlaan <nvanderlaan@mpr.org>
2016-07-19 13:15:10 -0500

    WIP: Add class comment for Rubocop

--- lib/generators/rateitapp/initializer_generator.rb
+++ lib/generators/rateitapp/initializer_generator.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 module Rateitapp
   module Generators
+    # Auto generate files in host app
     class InitializerGenerator < Rails::Generators::Base
       def create_initializer_file
         create_file 'config/initializers/rateit.rb', '# Add initialization content here'

5e7486c62ae66689ca9e72e1852990697e233290
William Johnston <wjohnston@mpr.org>
2018-01-11 12:55:26 -0600

    Fix Layout/EmptyLinesAroundArguments

--- lib/generators/rateitapp/install/install_generator.rb
+++ lib/generators/rateitapp/install/install_generator.rb
@@ -18,20 +18,16 @@ class PlaylistPlugin
   def initialize(service_name)
     @service_name = service_name
   end
-
   def name
     @service_name + '-song' # ratable_type
   end
-
   def valid?(rating)
     uri = URI('http://' + Rails.configuration.playlist_domain + '/api/v1/services/' + @service_name + '/songs/' + rating.ratable_id)
     res = Net::HTTP.get_response(uri)
-
     if res.is_a?(Net::HTTPSuccess)
       true
     else
       rating.errors.add(:ratable_id, 'ID is not valid')
-
       false
     end
   end
